21f772521a56fd561ef3de66c251e2003a853170
Russell Jarvis <colouredstatic@gmail.com>
2020-06-21 20:00:55 +1000

    web app made
    Former-commit-id: 3f52c593fa800e049c582c8cbab07a2e4c9569d3

--- t_analysis.py
+++ t_analysis.py
@@ -17,8 +17,8 @@ import sys
 import time
 import collections
 
-import matplotlib  # Its not that this file is responsible for doing plotting, but it calls many modules that are, such that it needs to pre-empt
-matplotlib.use('Agg')
+#import matplotlib  # Its not that this file is responsible for doing plotting, but it calls many modules that are, such that it needs to pre-empt
+#matplotlib.use('Agg')
 
 import numpy as np
 import pandas as pd
@@ -230,10 +230,11 @@ def text_proc(corpus, urlDat = {}, WORD_LIM = 100):
 
         return urlDat
 
+from tqdm import tqdm
 
 def process_dics(urlDats):
     dfs = []
-    for urlDat in urlDats:
+    for urlDat in tqdm(urlDats):
         # pandas Data frames are best data container for maths/stats, but steep learning curve.
         # Other exclusion criteria. Exclude reading levels above grade 100,
         # as this is most likely a problem with the metric algorithm, and or rubbish data in.
