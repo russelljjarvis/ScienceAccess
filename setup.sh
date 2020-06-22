af1fd814744c0731c10a327f42a9ad3ff24090c5
Russell Jarvis <rjjarvis@asu.edu>
2020-06-22 19:31:43 +1000

    contents

--- setup.sh
+++ setup.sh
@@ -14,13 +14,15 @@
 sudo apt-get update
 sudo apt-get install jq wget
 
-install_dir="."
+#install_dir="."
 json=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest)
 url=$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("linux64"))')
 curl -s -L "$url" | tar -xz
 chmod +x geckodriver
-sudo mv geckodriver "$install_dir"
-echo "installed geckodriver binary in $install_dir"
+sudo cp geckodriver .
+sudo cp geckodriver ./app
+
+#echo "installed geckodriver binary in $install_dir"
 
 #sudo bash gecko_install.sh
 
