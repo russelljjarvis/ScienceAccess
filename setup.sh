3951cc6d2d02270625826751f293076942c4ac5e
Russell Jarvis <rjjarvis@asu.edu>
2020-06-22 16:08:50 +1000

    changed

--- setup.sh
+++ setup.sh
@@ -13,34 +13,36 @@
 # required for selenium to drive a firefox browser.
 sudo apt-get update
 sudo apt-get install jq wget
+sudo bash gecko_install.sh
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-sudo apt-get install python-lxml
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-pip install -U pip
+#wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+#bash miniconda.sh -b -p $HOME/miniconda
+#export PATH="$HOME/miniconda/bin:$PATH"
+#sudo apt-get install python-lxml
+#hash -r
+#conda config --set always_yes yes --set changeps1 no
+#conda update -q conda
+#conda info -a
+#pip install -U pip
 
 
 
 sudo python3 -m pip install -r requirements.txt
-sudo python3 -m pip install PyPDF2
-sudo python3 -m pip install pycld2
-sudo python3 -m pip install nltk
-sudo python3 -m pip install selenium
-sudo python3 -m pip install delver
-sudo python3 -m pip install pdfminer
-sudo python3 -m pip install pyvirtualdisplay
-sudo python3 -m pip install textstat
-sudo python3 -m pip install fsspec>=0.3.3
-sudo python3 -m pip install textblob
-sudo python3 -m pip install twython
+sudo python3 -m pip install seaborn
+
+#sudo python3 -m pip install PyPDF2
+#sudo python3 -m pip install pycld2
+#sudo python3 -m pip install nltk
+#sudo python3 -m pip install selenium
+#sudo python3 -m pip install delver
+#sudo python3 -m pip install pdfminer
+#sudo python3 -m pip install pyvirtualdisplay
+#sudo python3 -m pip install textstat
+#sudo python3 -m pip install fsspec>=0.3.3
+#sudo python3 -m pip install textblob
+#sudo python3 -m pip install twython
 sudo python3 -c "import nltk; nltk.download('punkt')"
 sudo python3 -c "import nltk; nltk.download('stopwords')"
-sudo bash gecko_install.sh
 
 #git clone https://github.com/ckreibich/scholar.py.git
 #cd scholar.py
