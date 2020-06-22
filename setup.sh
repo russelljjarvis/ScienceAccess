16690670576001142402f3da2a18a5a8ef4c138a
Russell Jarvis <colouredstatic@gmail.com>
2020-06-22 14:38:12 +1000

    blah
    Former-commit-id: 34f4512a5a3f4840f21fd4240b35e7f226e67ceb

--- setup.sh
+++ setup.sh
@@ -49,3 +49,14 @@ wget https://www.dropbox.com/s/3h12l5y2pn49c80/traingDats.p?dl=0
 wget https://www.dropbox.com/s/crarli3772rf3lj/more_authors_results.p?dl=0
 wget https://www.dropbox.com/s/x66zf52himmp5ox/benchmarks.p?dl=0
  
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"rjjarvis@asu.edu\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
