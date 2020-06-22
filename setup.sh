63c3076107e698b6563d953a5b30c52a76c0386f
Russell Jarvis <rjjarvis@asu.edu>
2020-06-22 16:29:14 +1000

    changed
    Former-commit-id: 969210b2d26186fb8ceaa95a29ab8b0d5135e143

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ sudo bash gecko_install.sh
 sudo python3 -m pip install -r requirements.txt
 sudo python3 -m pip install seaborn 
 sudo python3 -m pip install bs4
-sudo python3 -m pip install natsort
+sudo python3 -m pip install natsort dask
 #sudo python3 -m pip install PyPDF2
 #sudo python3 -m pip install pycld2
 #sudo python3 -m pip install nltk
