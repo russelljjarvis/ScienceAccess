2aa279056d6502d36347a35e5930f79bd422b5c4
Russell Jarvis <colouredstatic@gmail.com>
2020-06-24 12:52:22 +1000

    contents
    Former-commit-id: 931e7d136c4e24551cbccbe914b34877142d65b9 [formerly aa7b9aa443c31ec859177665de661adaa4f9743e]
    Former-commit-id: 36e82e433a3dc1f09bea5b1161db751a183e3d08
    Former-commit-id: 7254ed8dd30e7b935efe6cf602cac3e1721f7a51

--- apple_setup.sh
+++ apple_setup.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
-
 # https://gist.github.com/mikesmullin/2636776
-#
-# download and install latest geckodriver for linux or mac.
-# required for selenium to drive a firefox browser.
-
 if [[ $(uname) == "Darwin" ]]; then
     ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)” 
-    sudo brew install jq wget firefox python-lxml
-
-
-    
+    brew install jq 
+    brew install wget 
+    brew install firefox 
+    brew install python-lxml
+	brew install chromium-chromedriver 
 elif [[ $(uname) == "Linux" ]]; then
     sudo apt-get update
     sudo apt-get install jq wget firefox
     sudo apt-get install python-lxml
     sudo apt-get install -y firefox
-    which firefox
-
-    #echo "installed geckodriver binary in $install_dir"
-
-
-
 else
     echo "can't determine OS"
     exit 1
 fi
 sudo bash gecko_install.sh
-
-
-
 sudo python3 -m pip install -r requirements.txt
 sudo python3 -m pip install seaborn bs4 natsort dask plotly
 sudo python3 -c "import nltk; nltk.download('punkt')"
 sudo python3 -c "import nltk; nltk.download('stopwords')"
-
-#git clone https://github.com/ckreibich/scholar.py.git
-
 wget https://www.dropbox.com/s/3h12l5y2pn49c80/traingDats.p?dl=0
 wget https://www.dropbox.com/s/crarli3772rf3lj/more_authors_results.p?dl=0
 wget https://www.dropbox.com/s/x66zf52himmp5ox/benchmarks.p?dl=0
 
-
+# sudo conda install -c plotly plotly=4.8.1
+# sudo python3 align_data_sources.py
