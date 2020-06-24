808d9f3f736146109ed8efd0ea9acf4065810c76
Russell Jarvis <rjjarvis@asu.edu>
2020-06-24 11:20:45 +1000

    contents
    Former-commit-id: 3e12ff73a3ff401c7dee36760f6976c855781027

--- app.py
+++ app.py
@@ -21,23 +21,7 @@ import plotly.express as px
 
 #from plotly.subplots import make_subplots
 
-import nltk
-try:
-    from nltk.corpus import stopwords
-    stop_words = stopwords.words('english')
-except:
-    nltk.download('punkt')
-    nltk.download('stopwords')
-
-if not(os.path.exists('traingDats.p?dl=0') or os.path.exists('data/traingDats.p')):
-
-    os.system('wget https://www.dropbox.com/s/3h12l5y2pn49c80/traingDats.p?dl=0')
-    os.system('wget https://www.dropbox.com/s/x66zf52himmp5ox/benchmarks.p?dl=0')
-
-if os.path.exists("traingDats.p?dl=0") and not os.path.exists("data/traingDats.p"):
-    os.system('mv traingDats.p?dl=0 data/traingDats.p')
-    os.system('mv benchmarks.p?dl=0 data/benchmarks.p')
-
+import nlkt
 
 trainingDats = pickle.load(open('data/traingDats.p','rb'))
 bio_chem = [ t['standard'] for t in trainingDats ]
