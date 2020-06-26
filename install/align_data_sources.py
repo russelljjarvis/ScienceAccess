d59ba9f9d499369099f2b558ab703954fa32595f
Russell Jarvis <colouredstatic@gmail.com>
2020-06-26 12:27:07 +1000

    blah
    Former-commit-id: 5fd5c7d0b839c295e8b9739dfa057c78f4fb6a3b
    Former-commit-id: 6d15355f8d4575e8a5f171ea21725496c09f935f

--- install/align_data_sources.py
+++ install/align_data_sources.py
@@ -1,13 +1,16 @@
 
 import os
-if not(os.path.exists('../data/traingDats.p')):
+if not(os.path.exists('data/traingDats.p')):
 
     os.system('wget https://www.dropbox.com/s/3h12l5y2pn49c80/traingDats.p?dl=0')
     os.system('wget https://www.dropbox.com/s/x66zf52himmp5ox/benchmarks.p?dl=0')
+    os.system('mv traingDats.p?dl=0 data/traingDats.p')
+    os.system('mv benchmarks.p?dl=0 data/benchmarks.p')
 
-if os.path.exists("traingDats.p?dl=0") and not os.path.exists("../data/traingDats.p"):
-    os.system('mv traingDats.p?dl=0 ../data/traingDats.p')
-    os.system('mv benchmarks.p?dl=0 ../data/benchmarks.p')
+
+if os.path.exists("data/traingDats.p?dl=0") and not os.path.exists("data/traingDats.p"):
+    os.system('mv traingDats.p?dl=0 data/traingDats.p')
+    os.system('mv benchmarks.p?dl=0 data/benchmarks.p')
 
 
 import nltk
