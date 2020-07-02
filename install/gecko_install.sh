8a8f3448064c96bf01fa70cfd1d01326f2043879
Russell Jarvis <rjjarvis@asu.edu>
2020-07-02 11:25:09 +1000

    contents
    Former-commit-id: fc89d7118a3d7c20f99cd70b209859f87d6854d8
    Former-commit-id: 02524e03788cd0d539221e3943eca96fd2eedcf8

--- install/gecko_install.sh
+++ install/gecko_install.sh
@@ -2,7 +2,6 @@
 # download and install latest geckodriver for linux or mac.
 # required for selenium to drive a firefox browser.
 
-install_dir="/usr/local/bin"
 json=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest)
 if [[ $(uname) == "Darwin" ]]; then
     url=$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("macos"))')
@@ -12,7 +11,18 @@ else
     echo "can't determine OS"
     exit 1
 fi
-curl -s -L "$url" | tar -xz
-chmod +x geckodriver
-sudo mv geckodriver "$install_dir"
-echo "installed geckodriver binary in $install_dir"
+
+if [[ -z "${DEPLOY_ENV}" ]]; then
+    install_dir="/usr/local/bin"
+    curl -s -L "$url" | tar -xz
+    chmod +x geckodriver
+    sudo mv geckodriver "$install_dir"
+    echo "installed geckodriver binary in $install_dir"
+
+else
+    install_dir="/home/circleci/project"
+    curl -s -L "$url" | tar -xz
+    chmod +x geckodriver
+    sudo mv geckodriver "$install_dir"
+    echo "installed geckodriver binary in $install_dir"
+fi
