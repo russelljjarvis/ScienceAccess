ef85a20a5605a252a0f94426d63d5dc09adf6a61
Russell Jarvis <rjjarvis@asu.edu>
2020-07-02 17:58:34 +1000

    contents
    Former-commit-id: d2bb4aafcb91130a9ad4f441e2073f1184e05d0a
    Former-commit-id: 22e5a7e1514bf8e2d8225b8522e9c863ab1ad5e6

--- science_access/crawl.py
+++ science_access/crawl.py
@@ -146,9 +146,15 @@ def get_driver():
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
+    if 'CIRCLE_BRANCH' in os.environ:
+        os.system("wget https://ftp.mozilla.org/pub/firefox/releases/45.0.2/linux-x86_64/en-GB/firefox-45.0.2.tar.bz2")
+        os.system("tar xvf firefox-45.0.2.tar.bz2")
+        options.binary_location = str('./firefox')
+
     try:
         if not os.path.exists("/usr/local/bin"):
-            driver = webdriver.Firefox(options=options)
+            driver = webdriver.Firefox(options=options)#,executable_path=GECKODRIVER_PATH)
+            
         else:
             GECKODRIVER_PATH=str("/usr/local/bin")+str("/geckodriver")
             driver = webdriver.Firefox(options=options,executable_path=GECKODRIVER_PATH)
@@ -173,7 +179,7 @@ def get_driver():
                     options.binary_location = str('./firefox')
                     driver = webdriver.Firefox(options=options,executable_path=GECKODRIVER_PATH)
                 except:
-                    os.system("wget wget https://ftp.mozilla.org/pub/firefox/releases/45.0.2/linux-x86_64/en-GB/firefox-45.0.2.tar.bz2")
+                    os.system("wget https://ftp.mozilla.org/pub/firefox/releases/45.0.2/linux-x86_64/en-GB/firefox-45.0.2.tar.bz2")
                     os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz")
                     os.system("tar -xf geckodriver-v0.26.0-linux64.tar.gz")
                     os.system("tar xvf firefox-45.0.2.tar.bz2")
