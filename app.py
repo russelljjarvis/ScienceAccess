8c7c31b80934b15b7b98ec6ba07394abacdfc160
Russell Jarvis <colouredstatic@gmail.com>
2020-08-04 14:41:22 +1000

    code quality improvements for pat
    Former-commit-id: a52ef12b7d618a6182a1881f93a01d31d71b6d0e
    Former-commit-id: b4b15e912e63ceace49cdb3e2218598a4b0774ae

--- app.py
+++ app.py
@@ -16,32 +16,27 @@ import math
 import scipy
 #from types import MethodType
 #from nltk import word_tokenize
+import plotly.graph_objects as go 
 
 
 from science_access.t_analysis import not_want_list
 from science_access.online_app_backend import call_from_front_end
 from science_access.online_app_backend import ar_manipulation
-from science_access.word_cloud_by_word_len import generate_from_lengths
+#from science_access.word_cloud_by_word_len import generate_from_lengths
 from science_access.utils import check_passive
 
 from science_access.enter_author_name import art_cloud, create_giant_strings
 from science_access.enter_author_name import distribution_plot_from_scrape, grand_distribution_plot
-from science_access.enter_author_name import push_frame_to_screen, fast_art_cloud
+from science_access.enter_author_name import push_frame_to_screen, fast_art_cloud, grab_data_for_splash
+from science_access.enter_author_name import frame_to_lists, try_and_update_cache, get_table_download_link, extra_options
 
-trainingDats = pickle.load(open('data/trainingDats.p','rb'))
-bio_chem = [ t['standard'] for t in trainingDats ]
-biochem_labels =  [ x['file_name'] for x in trainingDats if 'file_name' in x.keys()]
-biochem_labels = [x.split("/")[-1] for x in biochem_labels ]
-
-lods = []
-for i,j,k in zip(bio_chem,[str('Comparison Data') for i in range(0,len(bio_chem))],biochem_labels):
-     lods.append({'Reading_Level':i,'Origin':j,'Web_Link':k})
-df0 = pd.DataFrame(lods)
+with open('data/trainingDats.p','rb') as f:
+    trainingDats = pickle.load(f)
+    df0,bio_chem,biochem_labels = grab_data_for_splash(trainingDats)
 
 st.title('Search Reading Complexity of an Author')
 author_name = st.text_input('Enter Author Name:')
-#st.title('Optionally add number of documents to use')
-#ndocs = st.text_input('Enter Number of documents:, default 8')
+
 
 
 with open('data/_author_specificSayali Phatak.p','rb') as f:
@@ -50,18 +45,22 @@ with open('data/_author_specificSayali Phatak.p','rb') as f:
 cached_author_name = "Sayali Phatak"
 NBINS = 40
 
-def frame_to_lists(ar):
-    scraped_labels = [ str(x['link']) for x in ar]
-    standard_sci = [ t['standard'] for t in ar ]
-    return scraped_labels, standard_sci
 
 if author_name:
     ar = call_from_front_end(author_name)
-    df1,fig = distribution_plot_from_scrape(ar,author_name)
+    scraped_labels, standard_sci = frame_to_lists(ar)
+
+    df1,fig = distribution_plot_from_scrape(ar,author_name,scraped_labels,standard_sci,df0)
     st.write(fig)
     cached = False
+    # try and update underlying distribution with query, so information about science 
+    # is culmulative, dynamic.
+    # Try to allow researchers of the app to download the data.
+    # Via GUI prompts.
+    extra_options(ar,trainingDats,df1)
 else:
     cached = True
+    author_name = cached_author_name
     (ar, trainingDats) = ar_manipulation(ar)
 
     '''
@@ -69,8 +68,9 @@ else:
     '''
     scraped_labels, standard_sci = frame_to_lists(ar)
     push_frame_to_screen(scraped_labels,standard_sci)
-                                #ar,scraped_labels,standard_sci,df0,author_name
-    df1,fig = grand_distribution_plot(ar,scraped_labels,standard_sci,df0,author_name = cached_author_name)
+    st.markdown('-----')
+
+    df1,fig = grand_distribution_plot(ar,scraped_labels,standard_sci,df0,author_name = author_name)
     st.write(fig)
 
 
@@ -85,9 +85,8 @@ st.markdown('''
 ### The average reading level of these documents was {0}.
 
 '''.format(round(np.mean(standard_sci)),3))
-
+st.markdown('-----')
 st.markdown('\n\n')
-
 
 '''
 ### Links to articles obtained from the search.
@@ -97,11 +96,8 @@ push_frame_to_screen(scraped_labels,standard_sci)
 
 # Create a list of possible values and multiselect menu with them in it.
 
-
-
-st.markdown('')
-st.markdown('')
-
+st.markdown('-----')
+st.markdown('\n\n')
 '''
 ### These links are identified individually on the histogram below
 '''
@@ -113,6 +109,7 @@ st.markdown('''
 '''
 Here are a few additional established text sources of known complexity for comparison.
 '''
+st.markdown('-----')
 
 st.markdown(""" # Benchmarks in detail""")
 
@@ -131,32 +128,22 @@ st.markdown("""
 | [Post-Modern Essay Generator](http://www.elsewhere.org/journal/pomo/) | 16.5                          | -  |
 | [Art Corpus](https://www.aber.ac.uk/en/cs/research/cb/projects/art/art-corpus/)                                 | 18.68                        | 2,594 |
 """) 
-
-st.markdown('')
-st.markdown('')
-st.markdown('')
-st.markdown('')
+st.markdown('-----')
+st.markdown('\n\n\n\n')
 
 
 
 
 
 if np.mean(standard_sci) < np.mean(bio_chem):
-
-    '''
-    ### This author was on average easier to read relative to ART Corpus.
-
-    '''
+    st.markdown('''
+    ### {0} was on average easier to read relative to ART Corpus.
+    '''.format(author_name))
 
 if np.mean(standard_sci) >= np.mean(bio_chem):
-
-
-    '''
-    ### This author was on average more difficult to read relative to ART Corpus.
-
-    '''
-
-
+    st.markdown('''
+    ### {0} was on average more difficult to read relative to ART Corpus.
+    '''.format(author_name))
 
 
 
@@ -172,26 +159,23 @@ matrix_twosample = [
 fig = ff.create_table(matrix_twosample, index=True)
 
 
-'''
-A t-test was used to determine whether the reading level for the entered author was
+st.markdown('''
+A t-test was used to determine whether the reading level for the {0} was
 significantly different from that of ART Corpus.
-'''
+'''.format(author_name))
 st.write(fig)
 
 if twosample_results[1] >= .05:
-
-    '''
-    The reading complexity of the scraped author's text was not significantly different than that of ART Corpus.
-
-    '''
+    st.markdown('''
+    The reading complexity of {0} text was not significantly different than that of ART Corpus.
+    '''.format(author_name))
 if twosample_results[1] < .05:
-
-    '''
-    The reading complexity of the scraped author's text was significantly different than that of ART Corpus.
-
-    '''
+    st.markdown('''
+    The reading complexity of {0} text was significantly different than that of ART Corpus.
+    '''.format(author_name))
 
 st.markdown('\n\n')
+st.markdown('-----')
 
 """
 ### Here are some links where you can read about the readability metrics and the algorithms used to compute the metrics:
@@ -205,24 +189,24 @@ st.markdown('\n\n')
 
 
 """
-#### Here is a source on adult literacy:
+#### [Here is a source](http://nces.ed.gov/naal/pdf/2006470.pdf) about variation in adult literacy:
 """
 """
-Kutner M, Greenberg E, Baer J. National Assessment of Adult Literacy (NAAL): A First Look at the Literacy of America’s Adults in the 21st Century (NCES 2006-470). Washington, DC: National Center for Education Statistics; 2005. http://nces.ed.gov/naal/pdf/2006470.pdf.
+Kutner M, Greenberg E, Baer J. National Assessment of Adult Literacy (NAAL): A First Look at the Literacy of America’s Adults in the 21st Century (NCES 2006-470). Washington, DC: National Center for Education Statistics; 2005.
 """
 
 
 
 sci_corpus = create_giant_strings(ar,not_want_list)
 bio_corpus = create_giant_strings(trainingDats,not_want_list)
+st.markdown('-----')
 
 '''
 ### Word cloud based on the scraped texts
 '''
 fast_art_cloud(sci_corpus)
 
-st.markdown('')
-st.markdown('')
+st.markdown('\n\n')
 
 
 st.markdown(""" # Benchmarks in detail""")
@@ -230,7 +214,9 @@ st.markdown(""" # Benchmarks in detail""")
 st.markdown("""
 1.  [Upgoer 5](https://splasho.com/upgoer5/library.php) - a library using only the 10,000 most commonly occurring English words[2].
 2.  Wikipedia - a free, popular, crowdsourced encyclopedia that is generated from self-nominating volunteers. 
-3.  Postmodern Essay Generator (PMEG) - generates output consisting of sentences that obey the rules of written English, but without restraints on the semantic conceptual references [5].
+3.  Postmodern Essay Generator (PMEG) - generates output consisting of sentences that obey the rules of written English, but without 
+whith content whose meaning is random and or absent.
+restraints on the semantic conceptual references [5].
 4.  ART Corpus - a library of scientific papers published in The Royal Society of Chemistry (RSC) [1].
 """)    
 st.markdown("""
@@ -253,13 +239,14 @@ uniqueness=[]
 for block in trainingDats:
     uniqueness.append(block['uniqueness'])
     sentiment.append(block['sp'])
+temp = np.mean(sentiment)<np.mean([r['sp'] for r in ar])
 st.markdown("""
 # Sentiment:
-It is {0} tht the mean sentiment polarity this author is more upbeat than that of the average ARTCORPUS article:
-""".format(np.mean(sentiment)<np.mean([r['sp'] for r in ar])))
+It is {} that the mean sentiment polarity {} is more upbeat than that of the average ARTCORPUS article:
+""".format(temp,author_name))
 
-
-labels = ['This Author positive sentiment','ART Corpus positive sentiment']
+temp = '{0} positive sentiment'.format(author_name)
+labels = [temp,'ART Corpus positive sentiment']
 values = [np.mean([r['sp'] for r in ar]),np.mean(sentiment)]
 
 fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
@@ -267,12 +254,12 @@ st.write(fig)
 
 st.markdown("""
 # Uniqueness of words:
-It is {0} that the mean uniqueness/ratio of the words used in the ARTCORPUS, this gives an idea of 
+It is {0} that the mean uniqueness/ratio of {1} versus the words used in the ARTCORPUS, this gives an idea of 
 how boring or alternatively colorful each article was to read
-""".format(np.mean(uniqueness)<np.mean([r['uniqueness'] for r in ar])))
+""".format(np.mean(uniqueness)<np.mean([r['uniqueness'] for r in ar]),author_name))
 
 
-labels = ['This Author unique words ratio','ART Corpus unique words ratio']
+labels = ['{0} unique words ratio'.format(author_name),'ART Corpus unique words ratio']
 values = [np.mean([ r['uniqueness'] for r in ar]),np.mean(uniqueness)]
 
 fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
