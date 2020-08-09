9edaa381d6081fb245ae0aad1611915cc41c3c01
pmcg <34006725+mcgurrgurr@users.noreply.github.com>
2020-08-09 09:45:44 -0400

    Update app.py
    Former-commit-id: ef8b540653c395f64f3ed1de4666f94ddefed74b
    Former-commit-id: 4d75837164fa01644f6cdb07e450acb19422cef6

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+"""
+## Science Readability Project
+
+To ensure that writing is accessible to the general population, authors must consider the length of written text, as well as sentence structure, vocabulary, and other language features. While popular magazines, newspapers, and other outlets purposefully cater language for a wide audience, there is a tendency for academic writing to use more complex, jargon-heavy language.
+
+In the age of growing science communication, this tendency for scientists to use more complex language can carry over when writing in more mainstream media, such as blogs and social media. This can make public-facing material difficult to comprehend, undermining efforts to communicate scientific topics to the general public. While readability tools, such as Readable and Upgoer5 currently exist to report on readability of text, they report the complexity of only a single document. In addition, these tools do not focus on complexity in a more academic-type context.
+
+To address this, we created a tool that uses a data-driven approach to provide authors with insights into the readability of the entirety of their published scholarly work with regard to other text repositories. The tool first quantifies an existing text repository [@Soldatova:2007] with complexity shown to be comparable to that of other scientific journals. The tool subsequently uses this output as a reference to show how the readability of user-selected written work compares to this source.
+
+Ultimately, this tool will expand upon current readability metrics by computing a more detailed and comparative look at the complexity of written text. We hope that this will allow scientists and other experts to better monitor the complexity of their writing relative to other text types, leading to the creation of more accessible online material. And perhaps more broadly contribute to an improved global communication and understanding of complex topics.
+
+Author: [Russell Jarvis](https://github.com/russelljjarvis)\n
+Author: [Patrick McGurrin](https://github.com/mcgurrgurr)\n
+Source: [Github](https://github.com/russelljjarvis/ScienceAccess)
+"""
+
 import streamlit as st
 import os
 #import matplotlib.pyplot as plt
