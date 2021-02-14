109f276de18da6b00885fd9b38b203754ab54682
Russell Jarvis <russelljarvis@protonmail.com>
2021-02-14 22:52:42 +1100

    update
    Former-commit-id: 5c12c55bbb385f72ac40619c7196b2439c5c753f
    Former-commit-id: 81222831d270e1c79cd369efbce4250b472b14d7

--- orcid_scrape.py
+++ orcid_scrape.py
@@ -1,7 +1,9 @@
-import orcid
-api = orcid.PublicAPI(institution_key, institution_secret, sandbox=True)
-search_results = api.search('text:English', access_token=Token)
+#import orcid
+
+
+#api = orcid.PublicAPI(institution_key, institution_secret, sandbox=True)
+#search_results = api.search('text:English', access_token=Token)
 #While creating a search query, it is possible to use a generator in order to reduce time needed to fetch a record.
-search_results = api.search_generator('text:English',
-                                      pagination=20)
-first_result = next(search_results)
+#search_results = api.search_generator('text:English',
+#                                      pagination=20)
+#first_result = next(search_results)
