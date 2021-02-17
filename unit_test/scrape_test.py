72d1fc3a6b6f0798c79ade39babe7e4e2c1a2d4b
Russell Jarvis <russelljarvis@protonmail.com>
2021-02-18 00:13:08 +1100

    update everything

--- unit_test/scrape_test.py
+++ unit_test/scrape_test.py
@@ -1,7 +1,8 @@
-from science_access.crawl import collect_pubs
-from science_access.get_bmark_corpus import get_bmarks
 import nltk
 nltk.download('stopwords')
+
+from science_access.crawl import collect_pubs
+from science_access.get_bmark_corpus import get_bmarks
 scholar_link=str('https://duckduckgo.com/?q=%21scholar')+str("ninkum")+str("&va=z&t=hc")
 follow_links = collect_pubs(scholar_link)
 urlDats = get_bmarks()
