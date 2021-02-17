7874795182e1abd889a7b8ad4a944bb09d4f82fc
Russell Jarvis <russelljarvis@protonmail.com>
2021-02-17 23:50:04 +1100

    probably works
    Former-commit-id: c953573a761b30dec275f5139fcd4409978010de
    Former-commit-id: 0b05d0bad99ed2e8d8748fd43d5809470d26b925

--- unit_test/scrape_test.py
+++ unit_test/scrape_test.py
@@ -1,9 +1,9 @@
 from science_access.crawl import collect_pubs
 from science_access.get_bmark_corpus import get_bmarks
+import nltk
+nltk.download('stopwords')
 scholar_link=str('https://duckduckgo.com/?q=%21scholar')+str("ninkum")+str("&va=z&t=hc")
 follow_links = collect_pubs(scholar_link)
-print(follow_links)
 urlDats = get_bmarks()
 for ur in urlDats:
     assert ur['standard'] <90
-
