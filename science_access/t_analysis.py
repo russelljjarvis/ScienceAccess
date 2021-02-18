c42aaf28b79c7811d3fcb6ae36607ba97b4f3564
Russell Jarvis <russelljarvis@protonmail.com>
2021-02-18 23:47:24 +1100

    update

--- science_access/t_analysis.py
+++ science_access/t_analysis.py
@@ -319,8 +319,8 @@ def text_proc(corpus, urlDat={}, WORD_LIM=50):
             urlDat["gf"] = textstat.gunning_fog(corpus)
 
             # explanation of metrics
-            urlDat["standard"] = round(textstat.text_standard(corpus, float_output=True),3)
-            if urlDat["standard"] > 40:
+            urlDat["standard"] = textstat.text_standard(corpus, float_output=True)
+            if urlDat["standard"] > 40 or urlDat["standard"]==0:
                 left, right = complexityAlongtheText(corpus)
                 if right <= left and right != 0:
                     urlDat["standard"] = right
@@ -334,8 +334,8 @@ def text_proc(corpus, urlDat={}, WORD_LIM=50):
             #    urlDat["flesch_kincaid_grade"] = textstat.flesch_kincaid_grade(corpus)
             #except:
             #    pass
-            if urlDat["standard"]==0:
-                urlDat["standard"] = urlDat["flesch_kincaid_grade"]
+            #if urlDat["standard"]==0:
+            #    urlDat["standard"] = urlDat["flesch_kincaid_grade"]
             # if urlDat["flesch_kincaid_grade"] <= urlDat["standard"]:
             #    urlDat["standard"] = urlDat["flesch_kincaid_grade"]
 
