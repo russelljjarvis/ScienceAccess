6af5c99bf064da0932d6d69bcbd89ad867c80013
pmcg <34006725+mcgurrgurr@users.noreply.github.com>
2021-06-07 09:46:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -560,7 +560,7 @@ def main():
         scraped_labels_new, author_score = frame_to_lists(full_ar_new)
         df_author_new, merged_df = data_frames_from_scrape(
             full_ar_new, author_name, scraped_labels, author_score, art_df
-	)
+        )
 	
 	scraped_labels_new.extend(scraped_labels)
         st.markdown("# Full texts:")
