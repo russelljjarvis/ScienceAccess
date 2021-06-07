ba9d1504b04c869de3b5f4747023f353e91e138c
pmcg <34006725+mcgurrgurr@users.noreply.github.com>
2021-06-07 09:53:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -566,8 +566,8 @@ def main():
             scraped_labels_new.extend(scraped_labels)
             st.markdown("# Full texts:")
             st.write(df_author_new)
-            st.markdown("# Abstracts:") 
-	    st.write(df_author)	
+            st.markdown("# Abstracts:")
+	    st.write(df_author)
 		
         st.markdown("\n")
         st.markdown("-----")
