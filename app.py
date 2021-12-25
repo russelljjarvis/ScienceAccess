d6e44a556888822a94eb67d73159c0563c9998c4
Russell Jarvis <russelljarvis@protonmail.com>
2021-12-25 12:49:45 +1100

    update app

--- app.py
+++ app.py
@@ -156,8 +156,10 @@ def check_cache(author_name: str, verbose=0):
     aliases = semantic_scholar_alias(author_name)
     if type(aliases) is not None:
         if len(aliases):
-            st.success("Aliases found in semantic scholar API...")
-            st.success("Also try one of these alternative name queries: {0}".format(aliases))
+            try:
+                st.success("Aliases found in semantic scholar API... \n Also try one of these alternative name queries: {0}".format(aliases))
+            except:
+                pass
             #author_name = aliases[0]
 
     with shelve.open("data/fast_graphs_splash.p") as db:
